0feba938410326fd11eaaf576c7cbbbae385221c
rromero <rromero@heligrafics.net>
2024-07-20 09:24:46 -0400

    update(main validate-model): add info of type field and if related or no

--- validate_model/main.py
+++ validate_model/main.py
@@ -4,6 +4,7 @@ import psycopg2
 from dotenv import load_dotenv
 from colorama import init, Fore, Style
 from os import system
+
 system("clear")
 
 # Inicializa colorama
@@ -24,7 +25,8 @@ class ModelFieldVisitor(ast.NodeVisitor):
                     else:
                         field_name = stmt.targets[0].id
                         if not field_name.startswith('_'):
-                            fields.append(field_name)
+                            field_type = self.get_field_type(stmt.value)
+                            fields.append((field_name, field_type))
             if model_name:
                 self.models[model_name] = fields
 
@@ -37,6 +39,11 @@ class ModelFieldVisitor(ast.NodeVisitor):
             return (isinstance(base.value, ast.Name) and base.value.id == 'models' and base.attr == 'Model')
         return False
 
+    def get_field_type(self, node):
+        if isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute):
+            return node.func.attr
+        return None
+
 def extract_models_info(file_path):
     with open(file_path, 'r') as file:
         tree = ast.parse(file.read(), filename=file_path)
@@ -48,18 +55,45 @@ def extract_models_info(file_path):
 def check_columns_in_table(table_name, columns, connection):
     cursor = connection.cursor()
     cursor.execute(f"""
-        SELECT column_name
+        SELECT column_name, data_type
         FROM information_schema.columns
         WHERE table_name = %s;
     """, (table_name,))
 
     rows = cursor.fetchall()
-    existing_columns = [row[0] for row in rows]
+    existing_columns = {row[0]: row[1] for row in rows}
 
-    fields_exist = [col for col in columns if col in existing_columns]
-    no_fields = [col for col in columns if col not in existing_columns]
+    fields_exist = [col for col in columns if col[0] in existing_columns]
+    no_fields = [col for col in columns if col[0] not in existing_columns and not col[0].endswith('_ids')]
+    related_fields = [col for col in columns if col[0].endswith('_ids')]
+    
+    # Verificar tipo de datos
+    wrong_type_fields = []
+    for col in fields_exist:
+        field_name, expected_type = col
+        actual_type = existing_columns[field_name]
+        if not is_type_matching(expected_type, actual_type):
+            wrong_type_fields.append((field_name, expected_type, actual_type))
+    
+    return fields_exist, no_fields, wrong_type_fields, related_fields
 
-    return fields_exist, no_fields
+def is_type_matching(expected_type, actual_type):
+    # Mapa de equivalencias de tipos de datos entre Odoo y PostgreSQL
+    type_map = {
+        'Float': 'double precision',
+        'Integer': 'integer',
+        'Datetime': 'timestamp without time zone',
+        'Boolean': 'boolean',
+        'Char': 'character varying',
+        'Text': 'text',
+        'Selection': 'character varying',
+        'Many2one': 'integer',
+        'One2many': 'integer',
+        'Html': 'text',
+        #'Many2many': 'integer[]'
+        # Agrega más equivalencias aquí según sea necesario
+    }
+    return type_map.get(expected_type) == actual_type
 
 def get_module_path():
     base_path = os.getenv("BASE_PATH")
@@ -93,7 +127,7 @@ def get_module_path():
 
     # Clear the console
     os.system('clear' if os.name == 'posix' else 'cls')
-    len_text =f"{Style.BRIGHT}{Fore.GREEN}Module review: {Fore.YELLOW}{selected_module}"
+    len_text = f"{Style.BRIGHT}{Fore.GREEN}Module review: {Fore.YELLOW}{selected_module}"
     real_len = len(len_text.replace(Style.BRIGHT, "").replace(Fore.GREEN, "").replace(Fore.YELLOW, ""))
 
     # Print selected information with color and decoration
@@ -116,18 +150,32 @@ def process_models_in_directory(models_path, connection):
                 models_data = extract_models_info(file_path)
                 for model_name, variable_names in models_data.items():
                     table_name = model_name.replace('.', '_')
-                    fields_exist, no_fields = check_columns_in_table(table_name, variable_names, connection)
+                    fields_exist, no_fields, wrong_type_fields, related_fields = check_columns_in_table(table_name, variable_names, connection)
 
                     print(f"Nombre del modelo: {model_name}")
                     print(f"Nombre de la tabla: {table_name}")
                     print("Variables encontradas:")
                     for field in fields_exist:
-                        print(f"  - {field}")
+                        print(f"  - {field[0]}")
                     print(f"{Style.BRIGHT}{Fore.YELLOW}Variables no encontradas:{Style.RESET_ALL}")
                     for field in no_fields:
-                        print(f"  - {Style.BRIGHT}{Fore.MAGENTA}{field}{Style.RESET_ALL}")
+                        print(f"  - {Style.BRIGHT}{Fore.MAGENTA}{field[0]}{Style.RESET_ALL}")
+                    print(f"{Style.BRIGHT}{Fore.CYAN}Variables con tipo de dato incorrecto:{Style.RESET_ALL}")
+                    for field in wrong_type_fields:
+                        print(f"  - {Style.BRIGHT}{Fore.MAGENTA}{field[0]}{Style.RESET_ALL} (esperado: {field[1]}, actual: {field[2]})")
+                    print(f"{Style.BRIGHT}{Fore.BLUE}Campos relacionados:{Style.RESET_ALL}")
+                    for field in related_fields:
+                        print(f"  - {Style.BRIGHT}{Fore.BLUE}{field[0]}{Style.RESET_ALL}")
+                    print("\n")
                     print(f"Total encontradas: {len(fields_exist)}")
                     print(f"{Style.BRIGHT}{Fore.RED}Total no encontradas: {len(no_fields)}{Style.RESET_ALL}")
+                    print(f"{Style.BRIGHT}{Fore.CYAN}Total con tipo de dato incorrecto: {len(wrong_type_fields)}{Style.RESET_ALL}")
+                    print(f"{Style.BRIGHT}{Fore.BLUE}Total campos relacionados: {len(related_fields)}{Style.RESET_ALL}")
+                    
+                    # Pausar si hay campos que no se encuentran o tienen tipo de dato incorrecto
+                    if no_fields or wrong_type_fields:
+                        input(f"{Fore.RED}Por favor, revise los problemas y luego presione Enter para continuar...{Style.RESET_ALL}")
+                    
                     print("\n")
 
 def main():
